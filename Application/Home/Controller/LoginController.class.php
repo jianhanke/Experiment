7257e2dc15c7a87b23e76d8d9d9e5ea5f7160384
jianke <1340289@qq.com>
2019-09-24 22:46:41 +0800

    修改了NoVNC显示的长度

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -13,11 +13,11 @@ class LoginController extends Controller{
         $model=D('Student');
         $info=$model->check_Login();        
        if(isset($info)){
-                session('user_id',$info['sid']);
-                session('user_name',$info['sname']);
             $this->success('登录成功', U('Home/Index/index'));
+            session('user_id',$info['sid']);
+            session('user_name',$info['sname']);
         }else{
-            $this->error('密码错误',U('Home/Login/login'));
+            $this->error('账号或密码错误',U('Home/Login/login'));
         }
     }
 
@@ -27,11 +27,5 @@ class LoginController extends Controller{
         $this->success('退出成功',U('Login/login'));
 
     }
-
-
-
-   
-
-   
 
 }
