f80606be61afccb5bfa4b70c93719f584ad5e1c8
jianke <1340289@qq.com>
2019-10-22 13:49:31 +0800

    测试了compile

--- Application/Home/Controller/OnlineCompileController.class.php
+++ Application/Home/Controller/OnlineCompileController.class.php
@@ -13,15 +13,78 @@ class OnlineCompileController extends MyController{
 	public function startCompile(){
 
 		$id=I('post.id');
-		dump($id);
-		$myCode=I('post.myCode');
-		dump($myCode);
-		$myCode=htmlspecialchars_decode($myCode);
 
-		echo $myCode;
+		$myCode=I('post.myCode');
+
+		$myCode=htmlspecialchars_decode(html_entity_decode($myCode));
+
+		$myCode=str_replace("<p>","",$myCode);
+		$myCode=str_replace("</p>","\n",$myCode);
+		$myCode=str_replace("&nbsp; ",' ',$myCode);
+		$myCode=str_replace("<br/>","\n",$myCode);
+		$myCode=str_replace("&nbsp;"," ",$myCode);
+		dump($myCode);
 
 		$time=time();
-		dump($time);
+
+		$rootPath="/ceshi/";
+		$beforeFile=$rootPath.$id.'_'.$time.'.c';
+		$afterFile=$rootPath.$id.'_'.$time;
+
+		dump($beforeFile);
+		dump($afterFile);
+
+
+		$myfile = fopen("$beforeFile", "w");
+		fwrite($myfile, $myCode);
+		fclose($myfile);
+
+		exec("gcc $beforeFile -o $afterFile 2>&1",$out,$status);
+		for($i=0;$i<count($out);$i++){
+			$out[$i]=str_replace("$beforeFile:","",$out[$i]);
+		}
+		dump ($out);
+		
+		if(!empty($out)){
+			unlink($beforeFile);
+			return 0;
+		}
+		exec("$afterFile  2>&1",$out2,$status2);
+		dump($out2);
+		
+		unlink($beforeFile);
+		unlink($afterFile);
+		return 0;
+
+	}
+	public function ceshi(){
+
+		$id=I('post.id');
+
+		$ceshi=I('post.ceshi');
+		dump($ceshi);
+
+
+		$hidden=I('post.hidden');
+		dump($hidden);
+
+		dump($id);
+		$myCode=I('post.myCode');
+
+		$myCode=htmlspecialchars_decode(html_entity_decode($myCode));
+		$hidden=htmlspecialchars_decode(html_entity_decode($hidden));
+
+
+		$myCode=str_replace("<p>","",$myCode);
+		$myCode=str_replace("</p>","\n",$myCode);
+		$myCode=str_replace("&nbsp; ",' ',$myCode);
+		$myCode=str_replace("<br/>","\n",$myCode);
+		$myCode=str_replace("&nbsp;"," ",$myCode);
+		// $myCode=str_replace("\\n","\\\n",$myCode);
+		dump($myCode);
+
+		$time=time();
+
 		$rootPath="/ceshi/";
 		$beforeFile=$rootPath.$id.'_'.$time.'.c';
 		$afterFile=$rootPath.$id.'_'.$time;
@@ -31,9 +94,21 @@ class OnlineCompileController extends MyController{
 
 		exec("touch $beforeFile 2>&1",$out,$status);
 
-		dump("echo  \"$myCode\" >> $beforeFile 2>&1");
+		// dump("echo  '$myCode' >> $beforeFile");
+		// $info="echo"." '$myCode' "." >> $beforeFile";
+		// echo $info;
 
-		exec("echo  '$myCode' >> $beforeFile 2>&1" ,$out,$status);
+		$myfile = fopen("$beforeFile", "w");
+		fwrite($myfile, $myCode);
+		fclose($myfile);
+
+		$myfile = fopen('/ceshi/testfile.txt', "w");
+		fwrite($myfile, $hidden);
+		fclose($myfile);
+
+
+
+
 
 		// exec('echo  \'#include <stdio.h>
   //           int main() {
@@ -61,6 +136,10 @@ class OnlineCompileController extends MyController{
 		// return 0;
 
 	}
+
+
+
+
 
 
 }
--- Application/Runtime/Cache/Home/7ca312fce73ce5726648dc780b5cd1ea.php
+++ Application/Runtime/Cache/Home/7ca312fce73ce5726648dc780b5cd1ea.php
@@ -11,12 +11,25 @@
 	
 		<input type="" name="id" value="1">	
 		<script id="container"   name="myCode"  style="height: 100%;" scrolling="yes"> </script>
-		<input type="submit" value="提交" >  
+		<input type="submit" value="提交"  onclick="ceshi()">  
 
 	</form>
 
 </body>
 </html>
+<script>
+
+function ceshi (){
+    var info=document.getElementById("container").innerHTML;
+console.log(info);
+}
+
+var one=escape("<p>#include&lt;stdio.h&gt;</p><p>int main(){</p><p>printf(&quot;ceshi&quot;)</p><p>}</p>");
+var two=unescape(one);
+console.log(one);
+console.log(two);
+</script>
+
 <script type="text/javascript" src="/Experiment/Public/Home/js/Ueditor/ueditor.config.js"></script>
                         <!-- 编辑器源码文件 -->
             <script type="text/javascript" src="/Experiment/Public/Home/js/Ueditor/ueditor.all.js"></script>
